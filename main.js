1897449008bef04de03f5146df23fc8356b63138
syipmong <syipmong5@gmail.com>
2022-10-16 14:17:29 +0100

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-function add(){
-    
-    let todo = document.getElementsByClassName('footer')[0]
-    
-    todo = prompt()
+function addNew() {
+    var item = document.getElementById('new-todo').value
+    var text = document.createTextNode(item)
+    var newItem = document.createElement('li')
+    newItem.appendChild(text)
+
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
+    //checkbox.style.float = "right";
+    newItem.appendChild(checkbox);
+
+    x = document.getElementById('list');
+    x.appendChild(newItem);
+
+
 }
+
+//how to remove an element in a to-do list?
+// <
+// i className = "material-icons"
+// onClick = {
+//     () => handleDelete(i)
+// } > delete < /i>
+
+
+// const handleDelete = i => {
+//     const taskList = [...Tasks]
+//     taskList.splice(i, 1)
+//     setTasks(taskList)
+// }
